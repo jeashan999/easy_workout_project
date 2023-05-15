03f757d4d32bb971f6df9047d87b6c3bed3e2a3a
jeashan999 <jeashan999@gmail.com>
2023-05-16 00:30:13 +0530

    logo added to top

--- EassyWorkOut/Controllers/DashBoardViewController.swift
+++ EassyWorkOut/Controllers/DashBoardViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class DashBoardViewController: UIViewController{
     private let workoutsTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        let tableView = UITableView(frame: .zero,  style: .grouped)
+        tableView.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
         return tableView
     }()
     
@@ -21,7 +21,21 @@ class DashBoardViewController: UIViewController{
 
         workoutsTableView.delegate = self
         workoutsTableView.dataSource = self
+        configuration()
+        
+        workoutsTableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         // Do any additional setup after loading the view.
+    }
+    private func configuration(){
+        var image = UIImage(named: "logoName")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self ,action: nil),
+            
+        
+        ]
+        navigationController?.navigationBar.tintColor = .white
     }
     override func viewWillLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -31,14 +45,20 @@ class DashBoardViewController: UIViewController{
 }
  
 extension DashBoardViewController: UITableViewDelegate, UITableViewDataSource {
-    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) ->  Int{
-        return 5
+        return 1
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello world"
-        cell.backgroundColor = .red
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = "Hello world"
+//        cell.backgroundColor = .red
+//        return cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
